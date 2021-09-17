86ce688ac033e5771f7441d097ea5fe7c31d9c28
Courtney Wilson <cmwilson132@gmail.com>
2021-09-17 08:57:51 -0500

    commit tree 423bb487f718466c7d7af3f3f4f1d410fdc94ef9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user, include: :items 
+  end
+
+
+  private
+  
+  def user_params
+    params.permit(:username, :city)
+  end
+
+
 end
